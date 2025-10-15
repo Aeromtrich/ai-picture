735027162594d3dd3324a8a7681db6835a440a87
Aeromtrich <aeromtrich@gmail.com>
2025-10-15 16:45:38 +0800

    初始化项目 + 业务代码

---
+++ src/main/java/com/google/picturebackend/controller/MainController.java
@@ -0,0 +1,21 @@
+package com.google.picturebackend.controller;
+
+import com.google.picturebackend.common.BaseResponse;
+import com.google.picturebackend.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    /**
+     * 健康检查
+     */
+    @GetMapping("/health")
+    public BaseResponse<String> health() {
+        return ResultUtils.success("ok");
+    }
+
+}
